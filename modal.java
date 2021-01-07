8ba79709679c60a7f44ed92c138ce12efa3af402
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-01-07 20:21:49 +0900

    Create modal.java

---
+++ modal.java
@@ -0,0 +1,16 @@
+window.onload = function() {
+ 
+    function onClick() {  //onClick을 보이게 해주는 함수를 생성하였습니다
+        document.querySelector('.pop_wrap').style.display ='block';
+        document.querySelector('.backbg').style.display ='block';
+    }   
+    function offClick() {  //offClick을 안보이게 해주는 함수를 생성하였습니다
+        document.querySelector('.pop_wrap').style.display ='none';
+        document.querySelector('.backbg').style.display ='none';
+    }
+ 
+    document.getElementById('pop_link').addEventListener('click', onClick);
+    document.querySelector('.pop_close').addEventListener('click', offClick);
+    //닫는 링크를 클릭시 팝업창이 닫혀집니다.
+};
+
