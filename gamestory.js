d47f4da5bb726eb862908196b794d53cf838b6a3
임동균 <70128434+Dong-Es@users.noreply.github.com>
2021-01-13 22:22:58 +0900

    Create gamestory.js

---
+++ gamestory.js
@@ -0,0 +1,7 @@
+function youtubeFullscreen(url){  /*버튼을 클릭했을때 유튜브 풀스크린(Url)이 생기도록 합니다.*/
+
+    window.open(url,"","fullscreen,scrollbars")
+
+}
+/*유튜브를 풀스크린 팝업창을 뛰우기 위해서는 https://www.youtube.com/watch?v=Ha6Yp_QIwI0에 watch다음에 _popup을 추가한후
+링크를 가져와주면 됩니다.https://www.youtube.com/embed/Ha6Yp_QIwI0이런식으로 변하게 됩니다.*/
